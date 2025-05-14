665ceee251d064ac74a79d7d50f7063fffb6e5b7
Andres Quichimbo <44269393+AndresQuichimbo@users.noreply.github.com>
2025-05-14 07:29:32 +0100

    Update calculate_penman_monteith_pet.py

--- calculate_penman_monteith_pet.py
+++ calculate_penman_monteith_pet.py
@@ -7,14 +7,12 @@ from astral.sun import sun
 
 
 
-def Grid_ETp(fname_in, lat, lon, z=0.0):
+def csv_ETp(fname_in, lat, lon, z=0.0):
 	"""
-	lat: latitude
-	lon: longitude
+	lat: latitude [degree]
+	lon: longitude (degree)
+	z: elevation (m)
 	"""
-	data = Dataset(fname_in,'r')
-	
-	z = 1368
 	
 	a = LocationInfo()
 	a.name = 'WalnutGulch'
@@ -24,7 +22,6 @@ def Grid_ETp(fname_in, lat, lon, z=0.0):
 	a.timezone = 'US/Central'
 	a.elevation = z
 			
-	time_in = num2date(data['time'][:], units = data['time'].units, calendar = data['time'].calendar)
 	
 	time_in = time_in+pd.to_timedelta(Local_hour, 'H')
 	
@@ -50,71 +47,44 @@ def Grid_ETp(fname_in, lat, lon, z=0.0):
 		
 		idate = idate + pd.to_timedelta(1, 'd')
 	
-	# Create new netcdf
+
+def get_pet_PM(data, daylight_t=0.5):
+	"""
+	data: csv file containing all ERA-land data
+	"""
 	
-	dataset = Dataset(fname_out, 'w', format = 'NETCDF4_CLASSIC')
 	
-	dataset.createDimension('time', None)
+	Tdew = -273.15+data['d2m'].values # Dewpoint temperature in C
 	
-	dataset.createDimension('lat', len(data['latitude']))
+	Ta = -273.15+data['t2m'].values # Temperature in C
 	
-	dataset.createDimension('lon', len(data['longitude']))
+	ws_u = np.abs(data['u10'].values)) # Wind speed in m/s in x-direction
 	
-	#dataset.createDimension('grid', len(data['latitude'])*len(data['longitude']))
+	ws_v = np.abs(data['v10'].values # Wind speed in m/s in y-direction
 	
-	# Create coordinate variables for 4-dimensions
+	P = 0.001*data['sp'].values # Pressure in kPa
 	
-	time = dataset.createVariable('time', np.float64, ('time',))
+	Rns = (1e-6)*data['ssr'].values # Net solar radiation in MJ m**-2 n*-1
 	
-	lon = dataset.createVariable('lon', np.float64, ('lon',))
+	Rnl = (1e-6)*data['str'].values # Net thermal radiation in MJ m**-2 n*-1
 	
-	lat = dataset.createVariable('lat', np.float64, ('lat',))
+	ws = np.sqrt(np.power(ws_u,2)+np.power(ws_v,2))
 	
-	# Create the actual 4-d variable
+	# Calculate net radiation
+	Rn = Rns+Rnl
+	# calculate wind speed
+	ws = fpet.wind_2m_from_wz(ws, 10.)	
+	# Calculate Saturation vapour pressure [kPa]
+	svp = fpet.svp_from_t(Ta)
+	# Calculate Actual vapour pressure [kPa]
+	avp = fpet.svp_from_t(Tdew)
+	# Calculate Slope of saturation vapour pressure curve [kPa degC-1]
+	delta_svp = fpet.delta_svp(Ta)
+	# Calculate psy: Psychrometric constant [kPa deg C]
+	psy = 0.000665*P
 	
-	#pet = dataset.createVariable('pet', np.float32,('time','grid'))
+	shf = daylight_t*Rn
 	
-	pet = dataset.createVariable('pet', np.float32,('time','lat','lon'))
-	
-	time.units = 'hours since 1980-01-01 00:00:00'
-	
-	time.calendar = 'gregorian'
-	
-	for i in range(len(time_in)):
-	
-		Tdew = -273.15+np.array(data['d2m'][i,:,:])#.flatten() # Dewpoint temperature in C
+	ETo = fpet.PM_FAO_hourly(Rn, Ta, ws, svp, avp, delta_svp, psy, shf)
 		
-		Ta = -273.15+np.array(data['t2m'][i,:,:])#.flatten() # Temperature in C
-		
-		ws_u = np.abs(np.array(data['u10'][i,:,:]))#.flatten() # Wind speed in m/s in x-direction
-		
-		ws_v = np.abs(np.array(data['v10'][i,:,:]))#.flatten() # Wind speed in m/s in y-direction
-		
-		P = 0.001*np.array(data['sp'][i,:,:])#.flatten() # Pressure in kPa
-		
-		Rns = (1e-6)*np.array(data['ssr'][i,:,:])#.flatten() # Net solar radiation in MJ m**-2 n*-1
-		
-		Rnl = (1e-6)*np.array(data['str'][i,:,:])#.flatten() # Net thermal radiation in MJ m**-2 n*-1
-		
-		PEv = (1e-3)*np.array(data['pev'][i,:,:])#.flatten() # Potential evaporation in MJ m**-2 n*-1
-		
-		Ev = (1e-3)*np.array(data['e'][i,:,:])#.flatten() # Evaporation in MJ m**-2 n*-1
-		
-		ws = np.sqrt(np.power(ws_u,2)+np.power(ws_v,2))
-		
-		# Calculate net radiation
-		Rn = Rns+Rnl
-		# calculate wind speed
-		ws = fpet.wind_2m_from_wz(ws, 10.)	
-		# Calculate Saturation vapour pressure [kPa]
-		svp = fpet.svp_from_t(Ta)
-		# Calculate Actual vapour pressure [kPa]
-		avp = fpet.svp_from_t(Tdew)
-		# Calculate Slope of saturation vapour pressure curve [kPa degC-1]
-		delta_svp = fpet.delta_svp(Ta)
-		# Calculate psy: Psychrometric constant [kPa deg C]
-		psy = 0.000665*P
-		
-		shf = daylight_t[i]*Rn
-		
-		ETo = fpet.PM_FAO_hourly(Rn, Ta, ws, svp, avp, delta_svp, psy, shf)
+	return Eto
